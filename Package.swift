e45b2b6d92f7d0bb091b9a90ac657d2a906d247c
Nalou Nguyen <nalou.dev@gmail.com>
2025-01-20 09:26:45 +0700

    add Swinject

--- Package.swift
+++ Package.swift
@@ -14,11 +14,18 @@ let package = Package(
             name: "MVVMCModule",
             targets: ["MVVMCModule"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Swinject/Swinject.git", exact: "2.8.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MVVMCModule"),
+            name: "MVVMCModule",
+            dependencies: [
+                .product(name: "Swinject", package: "Swinject")
+            ]
+        ),
         .testTarget(
             name: "MVVMCModuleTests",
             dependencies: ["MVVMCModule"]),
---
+++ Sources/MVVMCModule/Common/Wrapping.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nalou Nguyen on 20/1/25.
+//
+
+import Foundation
