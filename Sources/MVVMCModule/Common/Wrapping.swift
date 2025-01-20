da64ba4aefd5f24fe391408e74fe537459d0c7bb
Nalou Nguyen <nalou.dev@gmail.com>
2025-01-20 09:31:58 +0700

    update code

--- Sources/MVVMCModule/Common/Wrapping.swift
+++ Sources/MVVMCModule/Common/Wrapping.swift
@@ -1,8 +1,12 @@
 //
-//  File.swift
-//  
+//  Wrapping.swift
+//
 //
 //  Created by Nalou Nguyen on 20/1/25.
 //
 
 import Foundation
+import Swinject
+
+public typealias Assembly = Swinject.Assembly
+public typealias Container = Swinject.Container
